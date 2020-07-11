694cae2bb511af4f4ea336fcc4cad057f9f556fe
IchHalt <firen8@hotmail.de>
2020-07-11 12:18:31 +0200

    Test

---
+++ gmtk-2020/Assets/Script/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Rigidbody2D rbEnemy;
+    Transform myTrans;
+    public float myWidth;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrans = this.transform;
+        rbEnemy = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 myVel = rbEnemy.velocity;
+        myVel.y = speed;
+        rbEnemy.velocity = myVel;
+    }
+}
---
+++ gmtk-2020/Assets/Script/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] spawns;
+    int randomSpawnPoint;
+    int randomSpawn;
+
+    public static bool spawnAllowed; // player alive
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnEnemy", 0f, 1f);
+        
+
+    }
+
+    void SpawnEnemy()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomSpawn = Random.Range(0, spawns.Length);
+            Instantiate(spawns[randomSpawn], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

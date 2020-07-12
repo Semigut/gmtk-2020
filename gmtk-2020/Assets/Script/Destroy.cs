5f7a90b39392161a86b37ca8235e0eeda6eb46d7
IchHalt <firen8@hotmail.de>
2020-07-12 14:33:20 +0200

    SoundTest Enemy

--- gmtk-2020/Assets/Script/Destroy.cs
+++ gmtk-2020/Assets/Script/Destroy.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    public AudioSource sound;
+    public AudioClip deadSound;
 
     public GameObject hitEffect;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sound = GetComponent<AudioSource>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -22,11 +23,13 @@ public class Destroy : MonoBehaviour
     {
         if(collision.gameObject.tag.Equals("Bullet"))
         {
+            sound.PlayOneShot(deadSound);
             Destroy(collision.gameObject);
             GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
             Destroy(effect, 0.35f);
             Score.scoreAmount += 1;
+           
         }
 
         
